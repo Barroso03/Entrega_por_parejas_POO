e4c34a96cb96387192f0eba74e60c0201e62793b
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-21 17:28:30 +0000

    creamos la clase palindromo

---
+++ clases/__pycache__/palindromo.py
@@ -0,0 +1,16 @@
+class palindromo():
+  def __init__(self,palabra)
+  self.palabra = palabra
+  
+
+
+
+  def palabrapalindromo(palabra):
+    palabra = input("Seleccione una palabra:")
+    if palabra == reverse(palabra):
+      print("True")
+    else:
+      print("False")
+print(palindromo.palabrapalindromo(palabra))
+
+    
