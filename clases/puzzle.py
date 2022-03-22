19d6c78ad72caf4481268356d98d4703ff8cbd76
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-22 07:57:42 +0000

    añadimos la clase logger

---
+++ clases/puzzle.py
@@ -0,0 +1,18 @@
+class A: 
+    def z(self): 
+        return self 
+ 
+    def y(self, t): 
+        return len(t) 
+ 
+a = A 
+y = a.z 
+print(y(a)) 
+aa = a() 
+print(aa is a()) 
+z = aa.y 
+print(z(())) 
+print(a().y((a,))) 
+print(A.y(aa, (a,z))) 
+print(aa.y((z,1,'z'))) 
+# Solucion: define dos funciones que son la de la Y y la de la Z y una variable que es la a, tras esto va operando con los resultados de la a, la Y y la Z
